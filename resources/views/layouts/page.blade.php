a761415e31977b733aedbba7cac8acf91f55c6bb
candra211204 <candraputra21ch@gmail.com>
2023-03-16 10:26:10 +0700

    .

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -22,6 +22,9 @@
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
     <link rel="stylesheet" href="{{ asset('layout/css/style.css') }}">
+
+    {{-- Font Awesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
@@ -37,30 +40,48 @@
         <a href="#" id="hamburger-menu">
             <i data-feather="menu">1</i>
         </a>
-        <a href="">
-            <i data-feather="search">1</i>
-            <span>Search</span>
-        </a>
-        <a href="">
-            <i data-feather="home">1</i>
-            <span>Home</span>
-        </a>
-        <a href="">
-            <i data-feather="mail">1</i>
-            <span>Mail</span>
-        </a>
+        @if (Auth::user())
+            <a href="">
+                <i data-feather="search">1</i>
+                <span>Search</span>
+            </a>
+            <a href="">
+                <i data-feather="home">1</i>
+                <span>Home</span>
+            </a>
+            <a href="">
+                <i data-feather="mail">1</i>
+                <span>Mail</span>
+            </a>
+        @endif
         @guest
             @if (Route::has('login') && Route::has('register'))
-                <a class="text-light m-3" href="{{ route('login') }}">
-                    <i data-feather="log-in">1</i>
+                <a class="text-light" href="{{ route('login') }}">
+                    <i class="fa-solid fa-house"></i>
                     <span>Login</span>
                 </a>
-                <a class="text-light m-3" href="{{ route('register') }}">
-                    <i data-feather="file-text">1</i>
+                <a class="text-light" href="{{ route('register') }}">
+                    <i class="fa-regular fa-house"></i>
                     <span>Register</span>
                 </a>
+                <a href="">
+                    <i data-feather="mail">1</i>
+                    <span>Carrier</span>
+                </a>
+                <a href="">
+                    <i data-feather="mail">1</i>
+                    <span>About</span>
+                </a>
+                <a href="">
+                    <i data-feather="mail">1</i>
+                    <span>Structure</span>
+                </a>
+                <a href="">
+                    <i data-feather="mail">1</i>
+                    <span>Mail</span>
+                </a>
             @endif
-        @else
+            @else
             <a class="text-light m-3" href="{{ route('logout') }}"
                 onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                 <i data-feather="log-out">1</i>
@@ -92,7 +113,7 @@
         @guest
             @if (Route::has('login') && Route::has('register'))
                 <a class="text-light m-3" href="{{ route('login') }}">
-                    <i data-feather="log-in">1</i>
+                    <i class="fa-solid fa-house m-2"></i>
                     <span>Login</span>
                 </a>
                 <a class="text-light m-3" href="{{ route('register') }}">
