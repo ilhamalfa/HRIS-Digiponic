a94818103639541abb6ba8d80f1dcb2709619e2a
asaddin02 <114129501+asaddin02@users.noreply.github.com>
2023-03-16 10:28:39 +0700

    sweetalert(arif)

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -97,6 +97,8 @@
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
 
+    {{-- sweetAlert --}}
+    @include('sweetalert::alert')
     {{-- JS --}}
     <script src="{{ asset('layout/js/script.js') }}"></script>
 
