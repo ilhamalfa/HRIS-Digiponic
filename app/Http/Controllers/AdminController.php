efc8d7dabf5adc7defc0ae58532cceb65fb5ebac
ilhamalfa <kuro.nekogami22@gmail.com>
2023-04-17 11:36:39 +0700

    search user

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -34,7 +34,7 @@ class AdminController extends Controller
     }
 
     public function dataUser(){
-        $datas = User::where('role', '!=', 'Pelamar')->paginate(10);
+        $datas = User::filter(request(['search']))->paginate(10);
 
         return view('super-admin.pegawai.daftar-data-user', [
             'datas' => $datas
--- resources/views/super-admin/pegawai/daftar-data-user.blade.php
+++ resources/views/super-admin/pegawai/daftar-data-user.blade.php
@@ -13,14 +13,25 @@
                     @if (Auth::user()->role != 'Admin')
                     <a href="{{ url('/data-user/input-user') }}" class="btn btn-primary mb-3">Add User</a>
                     @endif
+                    <div class="row mb-3 mx-1">
+                        <form class="nav-link d-none d-lg-flex search" action="{{ url('/data-user') }}">
+                            <div class="col-6">
+                                <input type="text" class="form-control text-white topbar-search-input" id="topbar-search-input" placeholder="Enter NIK or Name" name="search" autofocus>
+                            </div>
+                            <div class="col-2 mx-1">
+                                <button class="btn mt-1 w-75">Search</button>
+                            </div>
+                        </form>
+                    </div>
                     <table class="table mb-4">
                         <thead>
                             <tr>
                                 <th scope="col">#</th>
                                 <th scope="col">NIK</th>
+                                <th scope="col">Nama</th>
                                 <th scope="col">Email</th>
-                                <th scope="col">Email Verified</th>
-                                <th scope="col">Role</th>
+                                <th scope="col">Department</th>
+                                <th scope="col">Golongan</th>
                                 @if (Auth::user()->role != 'Admin')
                                 <th scope="col">Action</th>
                                 @endif
@@ -31,15 +42,10 @@
                             <tr>
                                 <th scope="row">{{ $loop->iteration }}</th>
                                 <td>{{ $data->nik }}</td>
+                                <td>{{ $data->nama }}</td>
                                 <td>{{ $data->email }}</td>
-                                <td>
-                                    @if ($data->email_verified_at == Null)
-                                        <h6 class="text-danger">Verify the email</h6>
-                                    @else
-                                        <h6 class="text-success">Verified</h6>
-                                    @endif
-                                </td>
-                                <td>{{ $data->role }}</td>
+                                <td>{{ $data->department }}</td>
+                                <td>{{ $data->golongan }}</td>
                                 <td>
                                     @if ($data->id != Auth::user()->id)
                                         <a href="{{ '/data-user/delete-user/' . $data->id }}" class="btn btn-danger" disabled>Delete</a>
